c9295809302418ce62f236a486062c50b4f7388d
Brett Viren <bv@bnl.gov>
2015-08-31 17:50:25 -0400

    Maybe a way to adapt between sig/slot and sink/source.

---
+++ inc/WireCellUtil/SSSSAdapter.h
@@ -0,0 +1,35 @@
+#ifndef WIRECELL_SSSSADAPTER
+#define WIRECELL_SSSSADAPTER
+
+namespace WireCell {
+
+    /** An adapter which drives the push-pull sink/process/source
+     * paradigm to the pure-pull signal/slot execution model.
+     */
+    template<typename InputType, typename OutputType, class Processor>
+    class SigSlotSinkSourceAdapter {
+    public:
+	typedef boost::signals2::signal<InputType ()> signal_type;
+	typedef signal_type::slot_type input_slot_type;
+
+	SigSlotSinkSourceAdapter(Processor& proc) : m_proc(proc) {}
+
+	OutputType operator()() {
+	    InputType input = m_proc();
+	    // fixme check returns....
+	    m_proc.sink(input);
+	    m_proc.process();
+	    OutputType ot;
+	    m_proc.source(ot);
+	    return ot;
+	}
+	
+	void connect(const input_slot_type& s) { m_input.connect(s); }
+
+    private:
+	Processor& m_proc;
+	signal_type m_input;
+    };
+
+}
+#endif
