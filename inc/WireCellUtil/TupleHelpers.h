ddd8a076e8d9f2f8a4c7b53e2ac308a143adc39f
Brett Viren <bv@bnl.gov>
2016-01-12 15:19:52 -0500

    Clean up a bit.

--- inc/WireCellUtil/TupleHelpers.h
+++ inc/WireCellUtil/TupleHelpers.h
@@ -1,13 +1,10 @@
 /** TupleHelpers help tuples help her make a great meal.
  *
- * These helpers provide template gymnastics to hide some complicated
- * code to access tuples more easily.
+ * These helpers provide template gymnastics to bridge compile-time
+ * and run-time access of the collection tuple's.  Compile-time
+ * elements are held as types in the tuple, run-time are held as
+ * boost::any in a std::vector<boost::any> or other collection.
  *
- * The doc strings on each helper assume an example tuple type like:
- *
- *     typedef std::tuple<int,float,double,char,std::string> IFDCS;
- *
- * For working examples, see test_tuple.cxx.
  */
 
 #ifndef WIRECELLUTIL_TUPLEHELPERS
@@ -24,126 +21,171 @@
 #include <utility>
 
 
+// much inspiration from:
+// https://www.preney.ca/paul/archives/486
+// http://loungecpp.wikidot.com/tips-and-tricks:indices
+// http://stackoverflow.com/questions/31463388/can-someone-please-explain-the-indices-trick
+
+/// If we are still before C++14, supply the fodder for doing the "indices trick".
+#if __cplusplus < 201402L
+namespace std {
+    template <std::size_t... Is>
+    struct index_sequence {};
+
+    template <std::size_t N, std::size_t... Is>
+    struct make_index_sequence : make_index_sequence<N-1, N-1, Is...> {};
+
+    template <std::size_t... Is>
+    struct make_index_sequence<0, Is...> : index_sequence<Is...> {};
+}
+#endif	// assume C++14
+
 namespace WireCell {
 
 
-    // much inspiration from:
-    // https://www.preney.ca/paul/archives/486
-    // http://loungecpp.wikidot.com/tips-and-tricks:indices
-    //
-    // fixme: the patterns of each helper differ as different tips
-    // were followed.  It would be good to clean this up.
-
-    /** Compile-time mapping of methods on tuple element types
+    /** A bag of functions to help deal with tuples at runtime.
      * 
-     *     std::vector<std::string> typenames = map_tuple< IFDCS >::type_names();
+     * The doc strings on each helper assume an example tuple type like:
+     *
+     *     typedef std::tuple<int,float,double,char,std::string> IFDCS;
+     *     tuple_helper<IFDCS> helper;
+     *
+     * For working examples, see test_tuple.cxx.
+     *
      */
     template <typename Tuple>
-    struct map_tuple;
+    struct tuple_helper;
+
     template<
 	template <typename...> class T,
-	typename Type,
 	typename... Types
 	>
-    struct map_tuple< T<Type, Types...> >
+    struct tuple_helper< T<Types...> >
     {
-	typedef T<Type, Types...> tuple_type;
+	typedef T<Types...> tuple_type;
 
-	static std::vector<std::string> type_names() {
-	    std::vector<std::string> ret = map_tuple< T<Types...> >::type_names();
-	    ret.insert(ret.begin(), std::string(typeid(Type).name()));
+	/// Make a new tuple type which each element is a container
+	/// holding type of original tuple element.
+	template< template <typename...> class Container >
+	struct Wrapped {
+	    typedef std::tuple<Container<Types>...> type;
+	};
+
+	typedef typename Wrapped<std::deque>::type queued_tuple_type;
+	typedef std::deque<boost::any> any_queue_type;
+	typedef std::vector<boost::any> any_vector_type;
+
+
+	/** Compile-time mapping of methods on tuple element types
+	 * 
+	 *     std::vector<std::string> typenames = helper.type_names();
+	 */
+	std::vector<std::string> type_names() {
+	    return { std::string(typeid(Types).name())... };
+	}
+
+
+	
+	// internal
+	template<std::size_t... Indices>
+	any_vector_type as_any_impl(const tuple_type& t, std::index_sequence<Indices...>) {
+	    return { boost::any(std::get<Indices>(t))... };
+	}
+
+	/** Convert a tuple of types to a vector of boost::any.
+	 *
+	 *     IFDCS ifdcs{1,2.2f,3.0e-9,'a',"foo"};
+	 *     std::vector<boost::any> anyvec = helper.as_any(ifdcs);
+	 */
+	any_vector_type as_any(const tuple_type& t) {
+	    return as_any_impl(t, std::make_index_sequence<std::tuple_size<tuple_type>::value>{});
+	}
+
+
+	// internal
+	template<std::size_t... Indices>
+	tuple_type from_any_impl(const any_vector_type& anyv, std::index_sequence<Indices...>) {
+	    return std::make_tuple(boost::any_cast<Types>(anyv[Indices])...);
+	}
+
+	/** Return a tuple made from the contents of an equivalent vector of boost::any
+	 *
+	 *  // anyvec is from above example
+	 *  IFDCS t = helper.from_any(anyvec);
+	 */
+	tuple_type from_any(const any_vector_type& anyv) {
+	    return from_any_impl(anyv, std::make_index_sequence<std::tuple_size<tuple_type>::value>{});
+	}
+
+
+	// internal
+	template<typename Element>
+	any_queue_type as_any_queue_convert(const std::deque<Element>& in) {
+	    return any_queue_type(in.begin(), in.end());
+	}
+
+	// internal
+	template<std::size_t... Indices>
+	std::vector< any_queue_type > as_any_queue_impl(const queued_tuple_type& toq, std::index_sequence<Indices...>) {
+	    return { as_any_queue_convert(std::get<Indices>(toq))... };
+	}
+
+	/** Convert a tuple of queues of types to a vector of queues of boost::any.
+	 *
+	 *     typedef typename tuple_helper<IFDCS>::Wrapped<std::deque>::type IFDCS_queues;
+	 *     IFDCS_queues qs;
+	 *     auto any_q = as_any_queue(qs);
+	 *     cout << "First element from each queue:\n";
+	 *     cout << any_cast<int>(any_q[0][0]) << endl;
+	 *     cout << any_cast<float>(any_q[1][0]) << endl;
+	 *     cout << any_cast<double>(any_q[2][0]) << endl;
+	 *     cout << any_cast<char>(any_q[3][0]) << endl;
+	 *     cout << any_cast<std::string>(any_q[4][0]) << endl;
+	 */
+	std::vector< any_queue_type > as_any_queue(const queued_tuple_type& toq) {
+	    return as_any_queue_impl(toq, std::make_index_sequence<std::tuple_size<tuple_type>::value>{});
+	}
+
+
+
+
+	// internal
+	template <typename Type>
+	std::deque<Type> from_any_queue_convert(const any_queue_type& aq) {
+	    std::deque<Type> ret;
+	    for (auto a : aq) {
+		ret.push_back(boost::any_cast<Type>(a));
+	    }
 	    return ret;
 	}
-    };
-    template<
-	template <typename...> class T,
-	typename Type
-	>
-    struct map_tuple< T<Type> >
-    {
-	static std::vector<std::string> type_names() {
-	    return std::vector<std::string>{std::string(typeid(Type).name())};
+
+	// internal
+	template<std::size_t... Indices>
+	queued_tuple_type from_any_queue_impl(const std::vector< any_queue_type >& vaq, std::index_sequence<Indices...>) {
+	    return std::make_tuple(from_any_queue_convert<Types>(vaq[Indices])...);
 	}
+
+	/** Convert a vector of queues of any to a tuple of queues of type.
+	 */
+	queued_tuple_type from_any_queue(const std::vector< any_queue_type >& vaq) {
+	    return from_any_queue_impl(vaq, std::make_index_sequence<std::tuple_size<tuple_type>::value>{});	    
+	}
+
     };
 
-	
-
-    /** Define a tuple-of-container-of-elements type from a tuple-of-elements.
+    // http://stackoverflow.com/a/20711990
+    /** Make an N-tuple of elements all the same type
      *
-     *     typedef typename wrap_tuple<IFDCS>::type ifdcs_queues;
-     *     ifdcs_queues my_tuple_of_queues;
+     * type_repeater<3, float>::type blah;
      */
-    template <
-	typename Tuple,
-	template <typename...> class Container = std::deque
-	>
-    struct wrap_tuple;
-    template<
-	template <typename...> class T,
-	template <typename...> class Container,
-	typename... Types
-	>
-    struct wrap_tuple< T<Types...>, Container >
-    {
-	typedef std::tuple<Container<Types>... > type;
+    template <std::size_t N, typename T>
+    struct type_repeater {
+	typedef decltype(std::tuple_cat(std::tuple<T>(), typename type_repeater<N-1, T>::type())) type;
     };
-
-
-    
-
-    // http://loungecpp.wikidot.com/tips-and-tricks:indices
-    // http://stackoverflow.com/questions/31463388/can-someone-please-explain-the-indices-trick
-    template <std::size_t... Indices>
-    struct indices {};
-
-    template <std::size_t N, std::size_t... Is>
-    struct build_indices : build_indices<N-1, N-1, Is...> {};
-
-    template <std::size_t... Is>
-    struct build_indices<0, Is...> : indices<Is...> {};
-	
-    template<typename Tuple, std::size_t... Indices>
-    std::vector<boost::any> as_any_detail(const Tuple& t, indices<Indices...>) {
-     	return { boost::any(std::get<Indices>(t))... };
-    }
-
-    /** Convert a tuple of types to a vector of boost::any.
-     *
-     *     IFDCS ifdcs{1,2.2,3.0e-9,'a',"foo"};
-     *     std::vector<boost::any> anyvec = as_any(ifdcs);
-     */
-    template <typename Tuple>
-    std::vector<boost::any> as_any(const Tuple&t) {
-    	return as_any_detail(t, build_indices<std::tuple_size<Tuple>::value>{});
-    }
-
-
-    template<typename Element>
-    std::deque<boost::any> as_any_queue_convert(const std::deque<Element>& in) {
-	return std::deque<boost::any>(in.begin(), in.end());
-    }
-
-    template<typename Tuple, std::size_t... Indices>
-    std::vector< std::deque<boost::any> > as_any_queue_detail(const Tuple& t, indices<Indices...>) {
-     	return { as_any_queue_convert(std::get<Indices>(t))... };
-    }
-
-    /** Convert a tuple of queues of types to a vector of queues of boost::any.
-     *
-     *     typedef typename wrap_tuple<IFDCS>::type ifdcs_queues;
-     *     ifdcs_queues my_tuple_of_queues;
-     *     auto any_q = as_any_queue(my_tuple_of_queues);
-     *     cout << "First element from each queue:\n";
-     *     cout << any_cast<int>(any_q[0][0]) << endl;
-     *     cout << any_cast<float>(any_q[1][0]) << endl;
-     *     cout << any_cast<double>(any_q[2][0]) << endl;
-     *     cout << any_cast<char>(any_q[3][0]) << endl;
-     *     cout << any_cast<std::string>(any_q[4][0]) << endl;
-     */
-    template <typename Tuple>
-    std::vector< std::deque<boost::any> > as_any_queue(const Tuple&t) {
-    	return as_any_queue_detail(t, build_indices<std::tuple_size<Tuple>::value>{});
-    }
+    template <typename T>
+    struct type_repeater<0,T> {
+	typedef decltype(std::tuple<>()) type;
+    };
 
 }
 
--- test/test_tuple.cxx
+++ test/test_tuple.cxx
@@ -3,32 +3,46 @@
 
 #include <iostream>
 
+using namespace WireCell;
+
+
+
+
+
+
+
 int main()
 {
-    using namespace WireCell;
 
     typedef std::tuple<int,float,double,char,std::string> IFDCS;
+    typedef tuple_helper<IFDCS> IFDCS_helper;
+    typedef IFDCS_helper::queued_tuple_type IFDCS_queues;
 
-    std::vector<std::string> typenames = map_tuple< IFDCS >::type_names();
+    IFDCS_helper ifdcs_helper;
+
+    std::vector<std::string> typenames = ifdcs_helper.type_names();
     for (auto tn : typenames) {
 	std::cerr << tn << std::endl;
     }
 
     IFDCS ifdcs{1,2.2,3.0e-9,'a',"foo"};
-    std::vector<boost::any> anyvec = as_any(ifdcs);
+    //std::vector<boost::any> anyvec = as_any(ifdcs);
+    std::vector<boost::any> anyvec = ifdcs_helper.as_any(ifdcs);
     std::cerr << boost::any_cast<int>(anyvec[0]) << std::endl;
     std::cerr << boost::any_cast<std::string>(anyvec[4]) << std::endl;
 
+    auto ifdcs2 = ifdcs_helper.from_any(anyvec);
 
-    typedef typename wrap_tuple<IFDCS>::type ifdcs_queues;
-    ifdcs_queues qs;
+
+    //typedef typename tuple_helper<IFDCS>::Wrapped<std::deque>::type ifdcs_queues;
+    IFDCS_queues qs;
     std::get<0>(qs).push_back(1);
     std::get<1>(qs).push_back(2.2f);
     std::get<2>(qs).push_back(3.0e-9);
     std::get<3>(qs).push_back('a');
     std::get<4>(qs).push_back(std::string("foo"));
 	
-    auto any_q = as_any_queue(qs);
+    auto any_q = ifdcs_helper.as_any_queue(qs);
     Assert (any_q.size() == 5);
     for (auto q : any_q) {
 	Assert(q.size() == 1);
@@ -54,7 +68,7 @@ int main()
     // for (auto a: as_any(std::get<0>(qs))) {
     // 	std::cerr << boost::any_cast<int>(a) << std::endl;
     // }
-    typedef typename wrap_tuple<IFDCS, std::vector >::type ifdcs_vectors;
+    typedef typename IFDCS_helper::Wrapped<std::vector>::type ifdcs_vectors;
     ifdcs_vectors vs;
     std::get<0>(vs).push_back(1);
     std::get<1>(vs).push_back(2.2);
@@ -63,5 +77,10 @@ int main()
     std::get<4>(vs).push_back(std::string("foo"));
 
 
+    type_repeater<3, std::string>::type ahahah("one","two","three");
+    std::cerr << std::get<0>(ahahah) << " "
+	      << std::get<1>(ahahah) << " "
+	      << std::get<2>(ahahah) << "\n";
+    
 
 }
