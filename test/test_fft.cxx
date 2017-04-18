9810a05575dc589e73206e9d7ea9306fea9dd8ce
Brett Viren <brett.viren@gmail.com>
2017-04-18 13:29:48 -0400

    Changes for plots shown to DUNE core computing group today.

--- test/test_fft.cxx
+++ test/test_fft.cxx
@@ -1,9 +1,13 @@
+// fixme: this test utterly fails to properly use the system of units!
+
 #include "WireCellUtil/Waveform.h"
 
 #include "WireCellUtil/Response.h"
 
 #include "TCanvas.h"
 #include "TH1F.h"
+#include "TLine.h"
+#include "TText.h"
 
 #include <iostream>
 #include <algorithm>
@@ -23,13 +27,16 @@ void draw_time_freq(TCanvas& canvas,
     Waveform::compseq_t spec = Waveform::dft(res);
     Waveform::realseq_t res2 = Waveform::idft(spec);
 
-    int nticks = res.size();
+    const int nticks = res.size();
+    cerr << "Drawing nticks=" << nticks << endl;
+
     TH1F h_wave("response",title.c_str(), nticks, domain.first, domain.second);
     TH1F h_wave2("response2",title.c_str(), nticks, domain.first, domain.second);
     h_wave2.SetLineColor(2);
 
     h_wave.SetXTitle("Time (microsecond)");
     h_wave.SetYTitle("Gain (mV/fC)");
+    h_wave.GetXaxis()->SetRangeUser(0,10.0);
 
     for (int ind=0; ind<nticks; ++ind) {
 	h_wave.SetBinContent(ind+1, res[ind]);
@@ -40,19 +47,23 @@ void draw_time_freq(TCanvas& canvas,
 
     const double tick = (domain.second-domain.first)/nticks;
 
-    TH1F h_mag("mag","Magnitude of Fourier transform of response", nticks, 0, 1/tick);
-    h_mag.SetYTitle("Power");
+    const double max_freq = 1/tick;
+    TH1F h_mag("mag","Magnitude of Fourier transform of response", nticks, 0, max_freq);
+    h_mag.SetYTitle("Amplitude");
     h_mag.SetXTitle("MHz");
 
-    TH1F h_phi("phi","Phase of Fourier transform of response", nticks, 0, 1/tick);
-    h_phi.SetYTitle("Power");
+    TH1F h_phi("phi","Phase of Fourier transform of response", nticks, 0, max_freq);
+    h_phi.SetYTitle("Amplitude");
     h_phi.SetXTitle("MHz");
 
     for (int ind=0; ind<nticks; ++ind) {
 	auto c = spec[ind];
+        const double freq = tick*ind;
 	h_mag.SetBinContent(ind+1, std::abs(c));
 	h_phi.SetBinContent(ind+1, std::arg(c));
     }
+    h_mag.GetXaxis()->SetRangeUser(0,2.0);
+    h_phi.GetXaxis()->SetRangeUser(0,2.0);
 
     canvas.Clear();
     canvas.Divide(2,1);
@@ -70,11 +81,27 @@ void draw_time_freq(TCanvas& canvas,
     pad->SetGridy();
     h_mag.Draw();
 
+    TLine ndb;
+    TText ndbtxt;
+    double maxmag = h_mag.GetMaximum();
+    double db3 = maxmag / 1.414;
+    double db6 = maxmag / 1.995;
+    double db10 = maxmag / 3.162;
+    double db20 = maxmag / 10;
+    ndb.DrawLine(0,db3, 1.0, db3);
+    ndb.DrawLine(0,db6,   1.0, db6);
+    ndb.DrawLine(0,db10,   1.0, db10);
+    ndb.DrawLine(0,db20,   1.0, db20);
+    ndbtxt.DrawText(0.5, db3, "-3dB");
+    ndbtxt.DrawText(0.5, db6, "-6dB");    
+    ndbtxt.DrawText(0.5, db10, "-10dB");    
+    ndbtxt.DrawText(0.5, db20, "-20dB");    
+
     pad = spad->cd(2);
     pad->SetGridx();
     pad->SetGridy();
     h_phi.Draw();
-    canvas.Print("test_fft.pdf",".pdf");
+    canvas.Print("test_fft.pdf","pdf");
 
 }
 
@@ -83,22 +110,22 @@ int main()
     const std::vector<double> gains = {7.8, 14.0}; // mV/fC
     const std::vector<double> shapings = {1.0, 2.0}; // microsecond
 
-    const Waveform::Domain domain(0.0, 10.0);
-    const double tick = 0.5;
+    const Waveform::Domain domain(0.0, 100.0); // us
+    const double tick = 0.1;    // us
     const int nticks = Waveform::sample_count(domain, tick);
-    
+    cerr << "Using nticks=" << nticks << endl;
 
-    const double tconst = 1000.0; // 1ms
+    const double tconst = 1000.0; // ms
 
     TCanvas canvas("test_fft", "Response Functions", 500, 500);
-    canvas.Print("test_fft.pdf[",".pdf");
+    canvas.Print("test_fft.pdf[","pdf");
 
     for (int ind=0; ind<gains.size(); ++ind) {
 	Response::ColdElec ce(gains[ind], shapings[ind]);
 	Waveform::realseq_t res = ce.generate(domain, nticks);
 
 	draw_time_freq(canvas, res,
-		       Form("Cold Electronics Response at %.0fus shaping", shapings[ind]), domain);
+		       Form("Cold Electronics Response at %.0fus peaking", shapings[ind]), domain);
     }
 
 
@@ -121,5 +148,5 @@ int main()
 		       "RC Response at 1ms time constant (suppress delta)", domain);
     }
 
-    canvas.Print("test_fft.pdf]",".pdf");
+    canvas.Print("test_fft.pdf]","pdf");
 }
