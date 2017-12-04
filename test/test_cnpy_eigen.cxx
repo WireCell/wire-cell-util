2fa834f4f62bbc966cf67ae7fef279a3b3da3d44
Brett Viren <brett.viren@gmail.com>
2017-12-04 09:21:36 -0500

    First commit of cnpy, allowing WCT to write Numpy array files.

---
+++ test/test_cnpy_eigen.cxx
@@ -0,0 +1,32 @@
+#include "WireCellUtil/cnpy.h"
+
+#include <Eigen/Core>
+
+typedef Eigen::Array<short, Eigen::Dynamic, Eigen::Dynamic> ArrayXXs;
+
+const int Nchanc = 480;
+const int Nchani = 800;
+const int Ntick = 2000;
+
+const char* out = "eigentest.npz";
+
+int main()
+{
+    //ArrayXXs a = ArrayXXs::Random(Nchanc,Ntick);
+    ArrayXXs a = ArrayXXs::Zero(Nchanc,Ntick);
+    a(0,1000) = 1000;
+    a(400,0) = 400;
+    const short* data = a.data();
+    cnpy::npz_save<short>(out, "a", data, {Ntick, Nchanc}, "w");
+    /*
+      >>> import numpy
+      >>> f = numpy.load("eigentest.npz")
+      >>> a = f['a']
+      >>> print a.shape, a[0,400], a[1000,0]
+      (2000, 480) 400 1000
+     */
+    return 0;
+}
+
+
+
