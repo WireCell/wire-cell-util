f1440c7adc4c05e0e1e655a9ea2b76ce030fc600
Brett Viren <brett.viren@gmail.com>
2019-04-17 16:33:42 -0400

    Shore up logging

--- inc/WireCellUtil/Logging.h
+++ inc/WireCellUtil/Logging.h
@@ -14,6 +14,7 @@
 
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
+
 #include <string>
 
 namespace WireCell {
--- src/Logging.cxx
+++ src/Logging.cxx
@@ -9,12 +9,35 @@
 using namespace WireCell;
 
 
+static
+Log::logptr_t wct_base_logger()
+{
+    const std::string name = "wct";
+    static Log::logptr_t base_logger = nullptr;
+    if (base_logger) {
+        return base_logger;
+    }
+    base_logger = spdlog::get(name);
+    if (base_logger) {
+        return base_logger;
+    }
+
+    std::vector<spdlog::sink_ptr> sv;
+    base_logger = std::make_shared<spdlog::logger>(name, sv.begin(), sv.end());
+    spdlog::register_logger(base_logger);
+    base_logger->debug("create default logger \"wct\"");
+    spdlog::set_default_logger(base_logger);
+
+    return base_logger;
+}
+
+
 void Log::add_sink(Log::sinkptr_t sink, std::string level)
 {
     if (!level.empty()) {
         sink->set_level(spdlog::level::from_str(level));
     }
-    spdlog::default_logger()->sinks().push_back(sink);
+    wct_base_logger()->sinks().push_back(sink);
 }
 void Log::add_file(std::string filename, std::string level)
 {
@@ -45,32 +68,22 @@ void Log::add_stderr(bool color, std::string level)
     }
 }
 
-static
-Log::logptr_t wct_base_logger()
-{
-    const std::string name = "wct";
-    auto l = spdlog::get(name);
-    if (!l) {
-        std::vector<spdlog::sink_ptr> sv;
-        l = std::make_shared<spdlog::logger>(name, sv.begin(), sv.end());
-    }
-    spdlog::set_default_logger(l);
-    return l;
-}
-
 Log::logptr_t Log::logger(std::string name)
 {
     wct_base_logger();          // make sure base logger is installed.
     auto l = spdlog::get(name);
     if (!l) {
-        auto& sinks = spdlog::default_logger()->sinks();
+        auto& sinks = wct_base_logger()->sinks();
         l = std::make_shared<spdlog::logger>(name,  sinks.begin(), sinks.end());
 
         // peak under the hood of spdlog.  We want shared loggers to
         // get configured with the default level.
         spdlog::details::registry::instance().initialize_logger(l);
-
+        if (!spdlog::get(name)) {
+            spdlog::register_logger(l);
+        }
     }
+
     return l;
 }
 
